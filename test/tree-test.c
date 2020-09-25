c087572e3536b186009d7845a9dfaa894653e9c6
Lars Lenckowski <larslenckowski@gmail.com>
2020-09-25 21:52:41 +0200

    update test to new api

--- test/tree-test.c
+++ test/tree-test.c
@@ -5,21 +5,21 @@
 #include <stdio.h>
 
 int main() {
-	assert(save_url("test", NULL) == 0);
-	assert(save_url("test2", NULL) == 0);
-	assert(save_url("bruh", NULL) == 0);
-	assert(save_url("1", NULL) == 0);
-	assert(save_url("2", NULL) == 0);
-	assert(save_url("3", NULL) == 0);
-	assert(save_url("aaaaaaaaaaaaaaa", NULL) == 0);
+	assert(save_url("test", NULL, 1) == 0);
+	assert(save_url("test2", NULL, 1) == 0);
+	assert(save_url("bruh", NULL, 1) == 0);
+	assert(save_url("1", NULL, 1) == 0);
+	assert(save_url("2", NULL, 1) == 0);
+	assert(save_url("3", NULL, 1) == 0);
+	assert(save_url("aaaaaaaaaaaaaaa", NULL, 1) == 0);
 
 	print_tree();
 
 	clean_tree();
 
 	fuse_ino_t first = 0, second = 0;
-	assert(save_url("duplicate", &first) == 0);
-	assert(save_url("duplicate", &second) == 0);
+	assert(save_url("duplicate", &first, 1) == 0);
+	assert(save_url("duplicate", &second, 1) == 0);
 	assert(first == second && first != 0 && second != 0);
 
 	print_tree();
