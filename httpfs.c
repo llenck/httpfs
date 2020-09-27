ce1a6346172ab846da69c4d01feeb745243f9c07
Lars Lenckowski <larslenckowski@gmail.com>
2020-09-27 23:12:48 +0200

    incomplete readdir implementation

--- httpfs.c
+++ httpfs.c
@@ -97,10 +97,87 @@ void httpfs_getattr(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info* fi) {
 	fuse_reply_attr(req, &st, 600.0);
 }
 
+struct dirbuf {
+	char* p;
+	size_t size;
+};
+
+// kinda stolen from fuses hello_ll.c, but there aren't many easy ways to do
+// this so I would've ended up with very similar code anyways
+int dirbuf_add(fuse_req_t req, struct dirbuf* b, const char* name,
+		fuse_ino_t ino)
+{
+	size_t offs = b->size;
+	b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
+	b->p = (char*)realloc(b->p, b->size);
+	if (b->p == NULL) {
+		free(b->p);
+		return -1;
+	}
+
+	struct stat st;
+	memset(&st, 0, sizeof(st));
+	httpfs_fill_stats(&st, ino, 0); // parent ino isn't used anyways, so use 0
+
+	fuse_add_direntry(req, b->p + offs, b->size - offs, name, &st,
+			  b->size);
+
+	return 0;
+}
+
+#define MIN(a, b) ({ \
+	__auto_type _a = a; __auto_type _b = b; \
+	_a < _b? _a : _b; \
+})
+
+void httpfs_readdir(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
+	struct fuse_file_info* fi)
+{
+	(void) fi;
+
+	if (ino == FUSE_ROOT_ID) {
+		struct dirbuf b = { NULL, 0 };
+
+		if (
+			dirbuf_add(req, &b, "get", GET_INODE) < 0 ||
+			dirbuf_add(req, &b, "post", POST_INODE) < 0 ||
+			dirbuf_add(req, &b, "head", HEAD_INODE) < 0 ||
+			dirbuf_add(req, &b, "put", PUT_INODE) < 0 ||
+			dirbuf_add(req, &b, "delete", DELETE_INODE) < 0)
+		{
+			fuse_reply_err(req, ENOMEM);
+			return;
+		}
+
+		// off should always be positive
+		if ((size_t)off >= b.size) {
+			fuse_reply_buf(req, NULL, 0);
+		}
+		else {
+			fuse_reply_buf(req, b.p + off, MIN(b.size - off, size));
+		}
+
+		free(b.p);
+	}
+	else if (inode_to_tld(ino) != NULL) {
+		fuse_reply_err(req, ENOSYS);
+	}
+	else {
+		if (get_inode_info(ino, NULL) != NULL) {
+			fuse_reply_err(req, ENOTDIR);
+		}
+		else {
+			fuse_reply_err(req, ENOENT);
+		}
+
+		return;
+	}
+}
+
 const struct fuse_lowlevel_ops httpfs_ops = {
 	.lookup		= httpfs_lookup,
 	.getattr	= httpfs_getattr,
-//	.readdir	= httpfs_readdir,
+	.readdir	= httpfs_readdir,
 //	.open		= httpfs_open,
 //	.read		= httpfs_read,
 };
